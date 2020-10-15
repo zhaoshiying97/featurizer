bb30ab8656af4bdd85b20bb85b481aeaef60c528
walkacross <yujiangallen@126.com>
2020-10-15 14:15:17 +0800

    add residual mean

--- featurizer/functors/journalhub.py
+++ featurizer/functors/journalhub.py
@@ -90,6 +90,20 @@ class ResidualReturnsStd(Functor):
         residual = residual.squeeze(-1).transpose(0,1)
         return tsf.rolling_std(residual, self._window)
 
+class ResidualReturnsMean(Functor):
+    """Idiosyncratic (returns) mean"""
+    def __init__(self, window_train=10, window_test=3, window=3):
+        self._window_train = window_train
+        self._window_test = window_test
+        self._window = window
+        
+    def forward(self, tensor_x, tensor_y):
+        if tensor_x.dim() < tensor_y.dim():
+            tensor_x = tensor_x.expand_as(tensor_y)
+        residual = calc_residual3d(tensor_x, tensor_y, window_train=self._window_train, window_test=self._window_test, keep_first_train_nan=True)
+        residual = residual.squeeze(-1).transpose(0,1)
+        return tsf.rolling_mean(residual, self._window)
+
 
 if __name__ == "__main__":
     import torch

0ebc76516317030d113cc5592763f5507fadc185
walkacross <yujiangallen@126.com>
2020-10-15 14:04:27 +0800

    add calc_residual_use_case

--- featurizer/functors/journalhub.py
+++ featurizer/functors/journalhub.py
@@ -17,6 +17,7 @@
 
 from featurizer.interface import Functor
 from featurizer.functions import time_series_functions as tsf
+from featurizer.functions.calc_residual import calc_residual3d
 
 class ReturnsRollingStd(Functor):
     def __init__(self, window):
@@ -73,6 +74,22 @@ class AmihudRatio(Functor):
         output = abs(tensor_x) / tensor_y
         #pdb.set_trace()
         return tsf.rolling_mean_(output, window=self._window)
+
+
+class ResidualReturnsStd(Functor):
+    """Idiosyncratic (returns) STD"""
+    def __init__(self, window_train=10, window_test=3, window=3):
+        self._window_train = window_train
+        self._window_test = window_test
+        self._window = window
+        
+    def forward(self, tensor_x, tensor_y):
+        if tensor_x.dim() < tensor_y.dim():
+            tensor_x = tensor_x.expand_as(tensor_y)
+        residual = calc_residual3d(tensor_x, tensor_y, window_train=self._window_train, window_test=self._window_test, keep_first_train_nan=True)
+        residual = residual.squeeze(-1).transpose(0,1)
+        return tsf.rolling_std(residual, self._window)
+
 
 if __name__ == "__main__":
     import torch

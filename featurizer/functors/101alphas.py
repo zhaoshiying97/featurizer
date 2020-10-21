540f516d66adbd08a7fdebb55c19f21f9c257427
FrancisLi196 <lixiang196@qq.com>
2020-10-21 16:20:02 +0800

    beautified tsf

--- featurizer/functors/101alphas.py
+++ featurizer/functors/101alphas.py
@@ -3,9 +3,7 @@ from featurizer.functions import time_series_functions as tsf
 import torch
 import numpy as np
 import pandas as pd
-# import featurizer.functions as func
 from featurizer.interface import Functor
-# from featurizer.utils import check_np_transform
 import featurizer.functors.talib as talib
 import featurizer.functors.volume_price as vp
 import featurizer.functors.journalhub as jf
@@ -22,14 +20,12 @@ class Alpha006(Functor):
 
 class Alpha009(Functor):
 
-
     def forward(self, close_ts):
         delta_close = tsf.diff(close_ts, 1)
         cond_1 = (tsf.rolling_max(delta_close, 5) > 0).squeeze()
         cond_2 = (tsf.rolling_max(delta_close, 5) < 0).squeeze()
         alpha = -1 * delta_close
         result = torch.where((cond_1 | cond_2),alpha,delta_close)
-
         return result
 
 
@@ -41,39 +37,34 @@ class Alpha010(Functor):
         cond_2 = (tsf.rolling_max(delta_close, 4) < 0).squeeze()
         alpha = -1 * delta_close
         result = torch.where((cond_1 | cond_2),alpha,delta_close)
-
         return result
 
 class Alpha9_10_customizable(Functor):
+
     def __init__(self,window):
         self.window = window
+
     def forward(self, close_ts):
         delta_close = tsf.diff(close_ts, 1)
         cond_1 = (tsf.rolling_max(delta_close, self.window) > 0).squeeze()
         cond_2 = (tsf.rolling_max(delta_close, self.window) < 0).squeeze()
         alpha = -1 * delta_close
         result = torch.where((cond_1 | cond_2),alpha,delta_close)
-
         return result
 
 
 class Alpha012(Functor):
 
     def forward(self, close_ts, volume_ts):
-
         return torch.sign(tsf.diff(volume_ts, 1)) * (-1 * tsf.diff(close_ts, 1))
 
 
 class Alpha021(Functor):
 
     def forward(self, close_ts, volume_ts):
-
-        cond_1 = (tsf.rolling_mean_(close_ts, 8) + tsf.rolling_std(close_ts, 8) < tsf.rolling_mean_(close_ts, 2)).squeeze() # 前8天平均收盘价+8天收盘价的标准差。如果小于2天收盘价的平均价，则返回-1（不投资）
-
+        cond_1 = (tsf.rolling_mean_(close_ts, 8) + tsf.rolling_std(close_ts, 8) < tsf.rolling_mean_(close_ts, 2)).squeeze() # 前8天平均收盘价+8天收盘价的标准差。如果小于2天收盘价的平均价，则返回-1（不投资
         cond_2 = (tsf.rolling_mean_(volume_ts, 20) / volume_ts < 1 ).squeeze()# t-1日交易量是否超过或等于前20日平均交易量，是，则返回1（投资），否则返回-1（不投资）
         cond_3 = (tsf.rolling_mean_(close_ts, 8) - tsf.rolling_std(close_ts, 8) < tsf.rolling_mean_(close_ts, 2)).squeeze()# 则判断8天平均收盘价-8天收盘价的标准差，是否大于2天收盘价的平均价,是则返回1（投资），否则返回-1（不投资）
-
-
         ones = torch.ones(close_ts.size()).squeeze()
         result = torch.where ((cond_1 | (cond_2 & cond_3)),-1*ones, ones)
         return result
@@ -99,6 +90,7 @@ class Alpha024(Functor):
 
 
 class Alpha024_customizable(Functor):
+
     def __init__(self,window):
         self.window=window
 
@@ -116,10 +108,8 @@ class Alpha046(Functor):
         cond_1 = inner < 0
         cond_2 = inner > 0.25
         alpha = (-1 * tsf.diff(close_ts).squeeze())
-
         ones=torch.ones(close_ts.size()).squeeze()
         result=torch.where((cond_1 | cond_2 ), -1*ones, alpha)
-
         return result
 
 
@@ -144,14 +134,12 @@ class Alpha051(Functor):
         ones=torch.ones(close_ts.size()).squeeze()
         cond=(inner < -0.05).squeeze()
         result=torch.where(cond,ones,alpha)
-
         return result
 
 
 class Alpha053(Functor):
 
     def forward(self, high_ts, low_ts, close_ts):
-
         inner = (close_ts - low_ts).squeeze()
         constant=0.0001*torch.ones(close_ts.size()).squeeze()
         cond=inner==0
@@ -168,6 +156,5 @@ class Alpha054(Functor):
         constant=0.0001*torch.ones(close_ts.size()).squeeze()
         cond=inner==0
         inner=torch.where(cond,constant,inner)
-
         result = -1 * ((low_ts - close_ts) * (open_ts ** 5)).squeeze() / (inner * (close_ts ** 5).squeeze())
         return result

fbcb0c78ecf8b4cc7e1178b67733ecc08c6d63f4
FrancisLi196 <lixiang196@qq.com>
2020-10-22 16:15:14 +0800

    all alpha55 added TODO:decay_linear fix rolling_corr(rank())

--- test/test_alpha101.py
+++ test/test_alpha101.py
@@ -1,3 +1,10 @@
 import torch
 import unittest
 import featurizer.functors.alpha101 as alpha
+
+
+test_rising = torch.range(1,100,1)
+test_falling = torch.range(100,1,-1)
+test_2d = torch.stack([test_rising,test_falling],dim=1)
+tester=alpha.Alpha002()
+print(tester.forward(test_2d,test_2d,test_2d))

bbe82b60b0982c33c39eae82bda05e45e44746e5
vwang <hq.vicky.w@gmail.com>
2020-11-06 11:01:17 +0800

    Added module create_test_data.py

---
+++ test/test_residuals/create_test_data.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Author: Huanqiu Wang
+# Created on: Nov 6, 2020
+
+'''
+This module contains the code that creates data for test cases, including
+    - data needed while running the unit tests
+    - the expected output data to be compared with dynamic outputs
+'''
+
+import pandas as pd
+import pickle
+import torch
+

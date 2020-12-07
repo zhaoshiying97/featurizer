86e1fc58e9c930756ad4b1717ab17b165c6c9fde
Bowen905 <hbw0015@163.com>
2020-12-07 14:25:34 +0800

    TA-lib

--- featurizer/functions/talib_functions.py
+++ featurizer/functions/talib_functions.py
@@ -39,6 +39,46 @@ def macd(tensor, fastperiod=12, slowperiod=26, signalperiod=9):
 
     return DIF, DEA, MACD
 
+
+def macdext(close_ts, fastperiod=12, fastmatype=0, slowperiod=26, slowmatype=0, signalperiod=9, signalmatype=0):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    DIF = close_df.apply(lambda x: talib.MACDEXT(x, fastperiod=12, fastmatype=0, slowperiod=26, slowmatype=0, signalperiod=9, signalmatype=0)[0])
+    DEA = close_df.apply(lambda x: talib.MACDEXT(x, fastperiod=12, fastmatype=0, slowperiod=26, slowmatype=0, signalperiod=9, signalmatype=0)[1])
+    MACD = close_df.apply(lambda x: talib.MACDEXT(x, fastperiod=12, fastmatype=0, slowperiod=26, slowmatype=0, signalperiod=9, signalmatype=0)[2])
+    
+    DIF_ts = torch.tensor(DIF.values, dtype=close_ts.dtype, device=close_ts.device)
+    DEA_ts = torch.tensor(DEA.values, dtype=close_ts.dtype, device=close_ts.device)
+    MACD_ts = torch.tensor(MACD.values, dtype=close_ts.dtype, device=close_ts.device)
+    
+    return DIF_ts, DEA_ts, MACD_ts
+
+
+def macdfix(close_ts, signalperiod=9): # fixed fastperiod=12 and slowperiod=26
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    DIF = close_df.apply(lambda x: talib.MACDFIX(x, signalperiod=9)[0])
+    DEA = close_df.apply(lambda x: talib.MACDFIX(x, signalperiod=9)[1])
+    MACD = close_df.apply(lambda x: talib.MACDFIX(x, signalperiod=9)[2])
+    
+    DIF_ts = torch.tensor(DIF.values, dtype=close_ts.dtype, device=close_ts.device)
+    DEA_ts = torch.tensor(DEA.values, dtype=close_ts.dtype, device=close_ts.device)
+    MACD_ts = torch.tensor(MACD.values, dtype=close_ts.dtype, device=close_ts.device)
+    
+    return DIF_ts, DEA_ts, MACD_ts
+
+
+def ppo(close_ts, fastperiod=12, slowperiod=26, matype=0):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    PPO = close_df.apply(lambda x: talib.PPO(x, fastperiod=12, slowperiod=26, matype=0))
+    PPO_ts = torch.tensor(PPO.values, dtype=close_ts.dtype, device=close_ts.device)
+    return PPO_ts
+
+
 def rsi(tensor, timeperiod):
     import talib
     tensor_np = tensor.cpu().detach().numpy()
@@ -68,3 +108,240 @@ def kdj(high_ts, low_ts, close_ts, fastk_period=9, slowk_period=3, slowd_period=
     D = tsf.ema(K, slowd_period).squeeze(-1)
     J = 3*K - 2*D
     return RSV, K, D, J
+
+def candleup(open_ts, close_ts, high_ts): 
+    output = torch.max(open_ts, close_ts)
+    output_ts = high_ts - output
+    return output_ts
+
+
+def candledown(open_ts, close_ts, low_ts):
+    output = torch.min(open_ts, close_ts)
+    output_ts = output - low_ts
+    return output_ts
+
+
+def atr(high_ts, low_ts, close_ts, timeperiod=14):
+    true_range = torch.max(high_ts, tsf.shift(close_ts, window=1)) - torch.min(low_ts, tsf.shift(close_ts, window=1))
+    atr = tsf.rolling_mean_(true_range, window=timeperiod)
+    return atr
+
+
+def natr(high_ts, low_ts, close_ts, timeperiod=14):
+    true_range = torch.max(high_ts, tsf.shift(close_ts, window=1)) - torch.min(low_ts, tsf.shift(close_ts, window=1))
+    TRange_max = tsf.rolling_max(true_range, window=timeperiod)
+    TRange_min = tsf.rolling_min(true_range, window=timeperiod)
+    natr = tsf.rolling_mean_((true_range - TRange_min) / (TRange_max - TRange_min), window=timeperiod)
+    return natr
+
+def dmi(high_ts, low_ts, close_ts, timeperiod=14):
+    up = high_ts - tsf.shift(high_ts, window=1)
+    down = tsf.shift(low_ts, window=1) - low_ts
+    zero = torch.zeros_like(high_ts, dtype=high_ts.dtype, device=high_ts.device)
+    PDM = torch.where(up>torch.max(down, zero), up, zero)
+    MDM = torch.where(down>torch.max(up, zero), down, zero)
+    TR14 = tsf.rolling_mean_(torch.max(high_ts, tsf.shift(close_ts, window=1)) - torch.min(low_ts, tsf.shift(close_ts, window=1)), window=14)
+    PDI = PDM / TR14[-1] * 100
+    MDI = MDM / TR14[-1] * 100
+    DX = torch.abs(PDI - MDI) / torch.abs(PDI + MDI)
+    ADX = tsf.ema(DX, window=timeperiod)
+    ADXR = tsf.ema(ADX, window=timeperiod)
+    return PDM, MDM, PDI, MDI, DX, ADX, ADXR
+
+
+def apo(close_ts, fastperiod=12, slowperiod=26, matype=0):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    apo = close_df.apply(lambda x: talib.APO(x, fastperiod=12, slowperiod=26, matype=0))
+    
+    apo_ts = torch.tensor(apo.values, dtype=close_ts.dtype, device=close_ts.device)
+    return apo_ts
+
+def cci(high_ts, low_ts, close_ts, timeperiod=14):
+    TP = (high_ts + low_ts + close_ts) / 3
+    cci = (TP - tsf.rolling_mean_(TP, window=timeperiod)) / (0.015 * tsf.rolling_std(TP, window=timeperiod))
+    return cci
+
+def cmo(close_ts, timeperiod=14):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    cmo = close_df.apply(lambda x: talib.CMO(x, timeperiod=14))
+    
+    CMO_ts = torch.tensor(cmo.values, dtype=close_ts.dtype, device=close_ts.device)
+    return CMO_ts
+
+def mfi(high_ts, low_ts, close_ts, total_turnover_ts, timeperiod=14):
+    TP = (high_ts + low_ts + close_ts) / 3
+    MF = TP * total_turnover_ts
+    zero = torch.zeros_like(high_ts)
+    PMF = torch.where(MF > tsf.shift(MF, window=1), MF, zero)
+    NMF = torch.where(MF < tsf.shift(MF, window=1), MF, zero)
+    MR = tsf.rolling_sum_(PMF, window=timeperiod) / tsf.rolling_sum_(NMF, window=timeperiod)
+    MFI = 100 - (100 / (1 + MR))
+    return MFI
+
+def stochrsi(close_ts, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    fastk = close_df.apply(lambda x: talib.STOCHRSI(x, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)[0])
+    fastd = close_df.apply(lambda x: talib.STOCHRSI(x, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0)[1])
+    fastk_ts = torch.tensor(fastk.values, dtype=close_ts.dtype, device=close_ts.device)
+    fastd_ts = torch.tensor(fastd.values, dtype=close_ts.dtype, device=close_ts.device)
+    return fastk_ts, fastd_ts
+
+def trix(close_ts, timeperiod=30):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    trix = close_df.apply(lambda x: talib.TRIX(x, timeperiod=30))
+    trix_ts = torch.tensor(trix.values, dtype=close_ts.dtype, device=close_ts.device)
+    return trix_ts
+
+def uos(high_ts, low_ts, close_ts, timeperiod1=7, timeperiod2=14, timeperiod3=28, timeperiod4=6):
+    TH = torch.max(high_ts, tsf.shift(close_ts, window=1))
+    TL = torch.min(low_ts, tsf.shift(close_ts, window=1))
+    ACC1 = tsf.rolling_sum_(close_ts-TL, window=timeperiod1) / tsf.rolling_sum_(TH-TL, window=timeperiod1)
+    ACC2 = tsf.rolling_sum_(close_ts-TL, window=timeperiod2) / tsf.rolling_sum_(TH-TL, window=timeperiod2)
+    ACC3 = tsf.rolling_sum_(close_ts-TL, window=timeperiod3) / tsf.rolling_sum_(TH-TL, window=timeperiod3)
+    UOS = (ACC1*timeperiod2*timeperiod3 + ACC2*timeperiod1*timeperiod3 + ACC3*timeperiod1*timeperiod2) * 100 / (timeperiod1*timeperiod2 + timeperiod1*timeperiod3 + timeperiod2*timeperiod3)
+    MAUOS = tsf.ema(UOS, window=timeperiod4)
+    return UOS, MAUOS
+
+def wr(high_ts, low_ts, close_ts, timeperiod=14):
+    HT = tsf.rolling_max(high_ts, window=timeperiod)
+    LT = tsf.rolling_min(low_ts, window=timeperiod)
+    WR = (HT - close_ts) / (HT - LT) * 100
+    return WR
+
+def dema(close_ts, timeperiod=30):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    dema = close_df.apply(lambda x: talib.DEMA(x, timeperiod=30))
+    dema_ts = torch.tensor(dema.values, dtype=close_ts.dtype, device=close_ts.device)
+    return dema_ts
+
+def HT_trendline(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    HT_trendline = close_df.apply(lambda x: talib.HT_TRENDLINE(x))
+    HT_trendline_ts = torch.tensor(HT_trendline.values, dtype=close_ts.dtype, device=close_ts.device)
+    return HT_trendline_ts
+
+def kama(close_ts, timeperiod=30):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    kama = close_df.apply(lambda x: talib.KAMA(x, timeperiod=30))
+    kama_ts = torch.tensor(kama.values, dtype=close_ts.dtype, device=close_ts.device)
+    return kama_ts
+
+def mama(close_ts, fastlimit=0.5, slowlimit=0.05):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    mama = close_df.apply(lambda x: talib.MAMA(x, fastlimit=0.5, slowlimit=0.05)[0])
+    fama = close_df.apply(lambda x: talib.MAMA(x, fastlimit=0.5, slowlimit=0.05)[1])
+    mama_ts = torch.tensor(mama.values, dtype=close_ts.dtype, device=close_ts.device)
+    fama_ts = torch.tensor(fama.values, dtype=close_ts.dtype, device=close_ts.device)
+    return mama_ts, fama_ts
+
+def midpoint(close_ts, timeperiod=14):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    midpoint = close_df.apply(lambda x: talib.MIDPOINT(x, timeperiod=14))
+    midpoint_ts = torch.tensor(midpoint.values, dtype=close_ts.dtype, device=close_ts.device)
+    return midpoint_ts
+
+def midprice(high_ts, low_ts, timeperiod=14):
+    data = (high_ts + low_ts) / 2
+    midprice = tsf.rolling_median(data, window=timeperiod)
+    return midprice
+
+def tema(close_ts, timeperiod=30):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    tema = close_df.apply(lambda x: talib.TEMA(x, timeperiod=30))
+    tema_ts = torch.tensor(tema.values, dtype=close_ts.dtype, device=close_ts.device)
+    return tema_ts
+
+def wma(close_ts, timeperiod=30):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    wma = close_df.apply(lambda x: talib.WMA(x, timeperiod=30))
+    wma_ts = torch.tensor(wma.values, dtype=close_ts.dtype, device=close_ts.device)
+    return wma_ts
+
+def ad(high_ts, low_ts, close_ts, volume_ts, fastperiod=3, slowperiod=10):
+    zero = torch.zeros_like(high_ts, dtype=high_ts.dtype, device=high_ts.device)
+    CLV = torch.where(high_ts == low_ts, zero, (2*close_ts-high_ts-low_ts)/(high_ts-low_ts))
+    AD = tsf.rolling_sum_(volume_ts*CLV, window=2)
+    AD[0] = CLV[0]
+    ADOSC = tsf.ema(AD, window=fastperiod) - tsf.ema(AD, window=slowperiod)
+    return AD, ADOSC
+
+def obv(close_ts, volume_ts):
+    zero = torch.zeros_like(volume_ts, dtype=volume_ts.dtype, device=volume_ts.device)
+    volume_new = torch.where(close_ts == tsf.shift(close_ts, window=1), zero, torch.where(close_ts > tsf.shift(close_ts, window=1), volume_ts, -volume_ts))
+    volume_new[0] = volume_ts[0]
+    OBV = tsf.rolling_sum_(volume_new, window=2)
+    return OBV
+
+def TSF(close_ts, timeperiod=14):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    TSF = close_df.apply(lambda x: talib.TSF(x, timeperiod=14))
+    TSF_ts = torch.tensor(tsf.values, dtype=close_ts.dtype, device=close_ts.device)
+    return TSF_ts
+
+def HT_dcperiod(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    HT_dcperiod = close_df.apply(lambda x: talib.HT_DCPERIOD(x))
+    HT_dcperiod_ts = torch.tensor(HT_dcperiod.values, dtype=close_ts.dtype, device=close_ts.device)
+    return HT_dcperiod_ts
+
+def HT_dcphase(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    HT_dcphase = close_df.apply(lambda x: talib.HT_DCPHASE(x))
+    HT_dcphase_ts = torch.tensor(HT_dcphase.values, dtype=close_ts.dtype, device=close_ts.device)
+    return HT_dcphase_ts
+
+def HT_phasor(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    inphase = close_df.apply(lambda x: talib.HT_PHASOR(x)[0])
+    quadrature = close_df.apply(lambda x: talib.HT_PHASOR(x)[1])
+    inphase_ts = torch.tensor(inphase.values, dtype=close_ts.dtype, device=close_ts.device)
+    quadrature_ts = torch.tensor(quadrature.values, dtype=close_ts.dtype, device=close_ts.device)
+    return inphase_ts, quadrature_ts
+
+def HT_sine(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    sine = close_df.apply(lambda x: talib.HT_SINE(x)[0])
+    leadsine = close_df.apply(lambda x: talib.HT_SINE(x)[1])
+    sine_ts = torch.tensor(sine.values, dtype=close_ts.dtype, device=close_ts.device)
+    leadsine_ts = torch.tensor(leadsine.values, dtype=close_ts.dtype, device=close_ts.device)
+    return sine_ts, leadsine_ts
+
+def HT_trendmode(close_ts):
+    import talib
+    close_np = close_ts.cpu().detach().numpy()
+    close_df = pd.DataFrame(close_np)
+    HT_trendmode = close_df.apply(lambda x: talib.HT_TRENDMODE(x))
+    HT_trendmode_ts = torch.tensor(HT_trendmode.values, dtype=close_ts.dtype, device=close_ts.device)
+    return HT_trendmode_ts
--- featurizer/functors/talib.py
+++ featurizer/functors/talib.py
@@ -95,6 +95,56 @@ class MACDRelated(Functor):
         norm_MACD_diff = torch.clamp(tsf.diff(MACD),min=-1, max=1).squeeze(-1)
         return norm_DIF, norm_DEA, norm_MACD, norm_DIF_diff, norm_DEA_diff, norm_MACD_diff
 
+class MACDEXTRelated(Functor):
+    
+    def __init__(self, fastperiod=12, fastmatype=0, slowperiod=26, slowmatype=0, signalperiod=9, signalmatype=0):
+        self.fastperiod = fastperiod
+        self.fastmatype = fastmatype
+        self.slowperiod = slowperiod
+        self.slowmatype = slowmatype
+        self.signalperiod = signalperiod
+        self.signalmatype = signalmatype
+    
+    def forward(self, tensor_close):
+        DIF, DEA, MACD = talib_func.macdext(tensor_close, fastperiod=self.fastperiod, fastmatype=self.fastmatype, slowperiod=self.slowperiod, slowmatype=self.slowmatype, signalperiod=self.signalperiod, signalmatype=self.signalmatype)
+        
+        norm_DIF = torch.clamp(DIF, min=-1, max=1).squeeze(-1)
+        norm_DEA = torch.clamp(DEA, min=-1, max=1).squeeze(-1)
+        norm_MACD = torch.clamp(MACD, min=-1, max=1).squeeze(-1)
+        
+        norm_DIF_diff = torch.clamp(tsf.diff(DIF), min=-1, max=1).squeeze(-1)
+        norm_DEA_diff = torch.clamp(tsf.diff(DEA), min=-1, max=1).squeeze(-1)
+        norm_MACD_diff = torch.clamp(tsf.diff(MACD), min=-1, max=1).squeeze(-1)
+        return norm_DIF, norm_DEA, norm_MACD, norm_DIF_diff, norm_DEA_diff, norm_MACD_diff
+
+class MACDFIXRelated(Functor):
+    
+    def __init__(self, signalperiod=9):
+        self.signalperiod = signalperiod
+    
+    def forward(self, tensor_close):
+        DIF, DEA, MACD = talib_func.macdfix(tensor_close, signalperiod=9)
+        
+        norm_DIF = torch.clamp(DIF, min=-1, max=1).squeeze(-1)
+        norm_DEA = torch.clamp(DEA, min=-1, max=1).squeeze(-1)
+        norm_MACD = torch.clamp(MACD, min=-1, max=1).squeeze(-1)
+        
+        norm_DIF_diff = torch.clamp(tsf.diff(DIF), min=-1, max=1).squeeze(-1)
+        norm_DEA_diff = torch.clamp(tsf.diff(DEA), min=-1, max=1).squeeze(-1)
+        norm_MACD_diff = torch.clamp(tsf.diff(MACD), min=-1, max=1).squeeze(-1)
+        return norm_DIF, norm_DEA, norm_MACD, norm_DIF_diff, norm_DEA_diff, norm_MACD_diff
+
+class PPO(Functor):
+    
+    def __init__(self, fastperiod=12, slowperiod=26, matype=0):
+        self.fastperiod = fastperiod
+        self.slowperiod = slowperiod
+        self.matype = matype
+    
+    def forward(self, tensor_close):
+        PPO = talib_func.ppo(tensor_close, fastperiod=self.fastperiod, slowperiod=self.slowperiod, matype=self.matype)
+        return PPO
+
 class KDJRelated(Functor):
     
     def __init__(self, fastk_period=9, slowk_period=3, slowd_period=3):
@@ -105,6 +155,14 @@ class KDJRelated(Functor):
     def forward(self, high, low, close):
         rsv,k,d,j = talib_func.kdj(high_ts=high,low_ts=low,close_ts=close, fastk_period=self.fastk_period, slowk_period=self.slowk_period, slowd_period=self.slowd_period)
         return rsv,k,d,j 
+
+class RSI(Functor):
+    def __init__(self, timeperiod):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor):
+        rsi_ts = talib_func.rsi(tensor, timeperiod=self.timeperiod).squeeze(-1)
+        return rsi_ts
 
 
 class DemeanedRSI(Functor):
@@ -124,6 +182,18 @@ class RSIROCP(Functor):
         rsi_ts = talib_func.rsi(tensor, timeperiod=self.timeperiod).squeeze(-1)
         rsi_pct_change = tsf.pct_change(rsi_ts+100)
         return rsi_pct_change
+
+class STOCHRSI(Functor):
+    
+    def __init__(self, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0):
+        self.timeperiod = timeperiod
+        self.fastk_period = fastk_period
+        self.fastd_period = fastd_period
+        self.fastd_matype = fastd_matype
+    
+    def forward(self, tensor_close):
+        fastk, fastd = talib_func.stochrsi(tensor_close, timeperiod=self.timeperiod, fastk_period=self.fastk_period, fastd_period=self.fastd_period, fastd_matype=self.fastd_matype)
+        return fastk, fastd
         
 
 class BBANDS(Functor):
@@ -153,6 +223,295 @@ class PriceVolume(Functor):
 
         pv = rocp * v_pct_change_atan
         return pv
+
+class PctChangeNight(Functor):
+    def __init__(self, window):
+        self._window = window
+        
+    def forward(self, open, close):
+        shift_tensor = tsf.shift(close, window=1)
+        diff = open - shift_tensor
+        output = diff.div(shift_tensor)
+        output_ts = tsf.pct_change(output, period=self._window)
+        return output_ts
+
+class PctChangeIntra(Functor):
+    def __init__(self, window):
+        self._window = window
+        
+    def forward(self, open, close):
+        diff = close - open
+        output = diff.div(open)
+        output_ts = tsf.pct_change(output, period=self._window)
+        return output_ts
+
+class CandleUp(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_open, tensor_close, tensor_high):
+        output = talib_func.candleup(tensor_open, tensor_close, tensor_high)
+        return output
+
+class CandleDown(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_open, tensor_close, tensor_low):
+        output = talib_func.candledown(tensor_open, tensor_close, tensor_low)
+        return output
+    
+class BIAS(Functor):
+    def __init__(self, window):
+        self._window = window
+        
+    def forward(self, tensor):
+        bias_ts = tensor / tsf.rolling_mean(tensor, window=self._window) - 1
+        return bias_ts
+
+class ATR(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        ATR = talib_func.atr(tensor_high, tensor_low, tensor_close, timeperiod=self.timeperiod)
+        return ATR
+
+class NATR(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        NATR = talib_func.natr(tensor_high, tensor_low, tensor_close, timeperiod=self.timeperiod)
+        return NATR
+
+class DMIRelated(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        PDM, MDM, PDI, MDI, DX, ADX, ADXR = talib_func.dmi(tensor_high, tensor_low, tensor_close, timeperiod=self.timeperiod)
+        return PDM, MDM, PDI, MDI, DX, ADX, ADXR
+
+class APO(Functor):
+    
+    def __init__(self, fastperiod=12, slowperiod=26, matype=0):
+        self.fastperiod = fastperiod
+        self.slowperiod = slowperiod
+        self.matype = matype
+    
+    def forward(self, tensor_close):
+        APO = talib_func.apo(tensor_close, fastperiod=self.fastperiod, slowperiod=self.slowperiod, matype=self.matype)
+        return APO
+
+class CCI(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        CCI = talib_func.cci(tensor_high, tensor_low, tensor_close, timeperiod=self.timeperiod)
+        return CCI
+
+class CMO(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        CMO = talib_func.cmo(tensor_close, timeperiod=self.timeperiod)
+        return CMO
+
+class MFI(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close, tensor_total_turnover):
+        MFI = talib_func.mfi(tensor_high, tensor_low, tensor_close, tensor_total_turnover, timeperiod=self.timeperiod)
+        return MFI
+
+class TRIX(Functor):
+    
+    def __init__(self, timeperiod=30):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        TRIX = talib_func.trix(tensor_close, timeperiod=self.timeperiod)
+        return TRIX
+
+class UOS(Functor):
+    
+    def __init__(self, timeperiod1=7, timeperiod2=14, timeperiod3=28, timeperiod4=6):
+        self.timeperiod1 = timeperiod1
+        self.timeperiod2 = timeperiod2
+        self.timeperiod3 = timeperiod3
+        self.timeperiod4 = timeperiod4
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        UOS, MAUOS = talib_func.uos(tensor_high, tensor_low, tensor_close, timeperiod1=self.timeperiod1, timeperiod2=self.timeperiod2, timeperiod3=self.timeperiod3, timeperiod4=self.timeperiod4)
+        return UOS, MAUOS
+
+class WR(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close):
+        WR = talib_func.wr(tensor_high, tensor_low, tensor_close, timeperiod=self.timeperiod)
+        return WR
+
+class DEMA(Functor):
+    
+    def __init__(self, timeperiod=30):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        DEMA = talib_func.dema(tensor_close, timeperiod=self.timeperiod)
+        return DEMA
+
+class EMA(Functor):
+    
+    def __init__(self, window):
+        self.window = window
+    
+    def forward(self, tensor_close):
+        EMA = tsf.ema(tensor_close, window=self.window)
+        return EMA
+
+class HT_TrendLine(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        HT_TrendLine = talib_func.HT_trendline(tensor_close)
+        return HT_TrendLine
+
+class KAMA(Functor):
+    
+    def __init__(self, timeperiod=30):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        KAMA = talib_func.kama(tensor_close, timeperiod=self.timeperiod)
+        return KAMA
+
+class MAMA(Functor):
+    
+    def __init__(self, fastlimit=0, slowlimit=0):
+        self.fastlimit = fastlimit
+        self.slowlimit = slowlimit
+    
+    def forward(self, tensor_close):
+        MAMA, FAMA = talib_func.mama(tensor_close, fastlimit=self.fastlimit, slowlimit=self.slowlimit)
+        return MAMA, FAMA
+
+class MIDPOINT(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        MIDPOINT = talib_func.midpoint(tensor_close, timeperiod=self.timeperiod)
+        return MIDPOINT
+
+class MIDPRICE(Functor):
+    
+    def __init__(self, timeperiod=14):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_high, tensor_low):
+        MIDPRICE = talib_func.midprice(tensor_high, tensor_low, timeperiod=self.timeperiod)
+        return MIDPRICE
+
+class TEMA(Functor):
+    
+    def __init__(self, timeperiod=30):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        TEMA = talib_func.tema(tensor_close, timeperiod=self.timeperiod)
+        return TEMA
+
+class WMA(Functor):
+    
+    def __init__(self, timeperiod=30):
+        self.timeperiod = timeperiod
+    
+    def forward(self, tensor_close):
+        WMA = talib_func.wma(tensor_close, timeperiod=self.timeperiod)
+        return WMA
+
+class ADRelated(Functor):
+    
+    def __init__(self, fastperiod=3, slowperiod=10):
+        self.fastperiod = fastperiod
+        self.slowperiod = slowperiod
+    
+    def forward(self, tensor_high, tensor_low, tensor_close, tensor_volume):
+        AD, ADOSC = talib_func.ad(tensor_high, tensor_low, tensor_close, tensor_volume, fastperiod=self.fastperiod, slowperiod=self.slowperiod)
+        return AD, ADOSC
+
+class OBV(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close, tensor_volume):
+        OBV = talib_func.obv(tensor_close, tensor_volume)
+        return OBV
+
+class HT_DCPERIOD(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        HT_DCPERIOD = talib_func.HT_dcperiod(tensor_close)
+        return HT_DCPERIOD
+
+class HT_DCPHASE(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        HT_DCPHASE = talib_func.HT_dcphase(tensor_close)
+        return HT_DCPHASE
+
+class HT_PHASOR(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        inphase, quadrature = talib_func.HT_phasor(tensor_close)
+        return inphase, quadrature
+    
+
+class HT_SINE(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        sine, leadsine = talib_func.HT_sine(tensor_close)
+        return sine, leadsine
+
+class HT_TRENDMODE(Functor):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, tensor_close):
+        HT_TRENDMODE = talib_func.HT_trendmode(tensor_close)
+        return HT_TRENDMODE
 
 if __name__ == "__main__":
     pass
